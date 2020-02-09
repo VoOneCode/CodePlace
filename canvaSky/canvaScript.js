e165f6d873a5dec9a34c4d1359c8f6771020835e
VoOneCode <mr.vladimir.fedorov@gmail.com>
2020-02-09 19:16:22 +0300

    Create canvaScript.js
    -stars on sky
    -moon
    -littleBear

---
+++ canvaSky/canvaScript.js
@@ -0,0 +1,82 @@
+
+let
+  canv = document.querySelector('#canvas'),
+  ctx = canv.getContext('2d');
+
+
+
+  degree = Math.PI / 180;
+
+canv.height = window.innerHeight;
+canv.width = window.innerWidth;
+
+ctx.fillStyle = 'wheat';
+ctx.strokeStyle= 'white';
+
+ctx.save();
+
+//moon. add correct to positioning it
+function Moon(r, st_ecl, end_ecl){
+  let radius = r || 22,
+      start_eclipse = st_ecl || 0.01;
+      end_eclipse = end_ecl || 0.1;
+      //moving moon throw sky
+  //ctx.rotate(57 * degree);
+  ctx.beginPath();
+  ctx.arc(185, 75, radius, start_eclipse*degree, end_eclipse*degree, true);
+  ctx.closePath();
+  ctx.fill();
+  ctx.restore();
+};
+
+function Stars(rowX, rowY, stepX, stepY){
+  let x = rowX || 50;
+  let cur = 1;
+  while (x < canv.width) {
+    let y = rowY || 50;
+    for(y; y<canv.height; y+=stepY){
+      if( cur % 2 == 0){
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(++x, ++y);
+        ctx.closePath();
+        ctx.stroke();
+        x -= 1;
+        y += 16;
+    }
+      else{
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(++x, ++y);
+        ctx.closePath();
+        ctx.stroke();
+        x -= 1;
+        y += 14;
+    };
+    }
+    x += stepX;
+    cur +=1;
+};
+};
+
+function SmallBear(){
+  ctx.save();
+  ctx.rotate(-20*degree)
+  let sX = 120,
+      sY = 500;
+  ctx.strokeStyle = 'blue';
+  ctx.beginPath();
+  ctx.moveTo(sX, sY);
+  ctx.lineTo(sX-35, sY+30);
+  ctx.lineTo(sX, sY+90);
+  ctx.lineTo(sX+50, sY+80);
+  ctx.lineTo(sX, sY);
+  ctx.lineTo(sX-10, sY-120);
+  ctx.closePath();
+  ctx.stroke();
+  ctx.restore();
+}
+
+Moon(29, 290, 90);
+Stars(-2, -165, 35, 35);
+SmallBear();
